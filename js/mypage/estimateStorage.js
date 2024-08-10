b498eafd1d155423d86cd9dfc4c95fb29a33f385
JeongHwa518 <jeonghwa485@gmail.com>
2024-08-10 23:43:25 +0900

    [FE] : 회원가입 페이지 1차 완성

--- js/mypage/estimateStorage.js
+++ js/mypage/estimateStorage.js
@@ -2,28 +2,42 @@
 document.addEventListener("DOMContentLoaded", function () {
     const modal = document.querySelector(".modal");
     const closeBtn = document.querySelector(".modal-close-btn");
-    const modalOn = document.querySelector("#icon");
 
-    modalOn.addEventListener("click", function() {
-        modal.classList.add("show");
+    document.querySelectorAll("#icon").forEach((icon) => {
+        icon.addEventListener("click", () => {
+            modal.classList.add("show");
+        })
     });
-
+    
     closeBtn.addEventListener("click", function() {
         modal.classList.remove("show");
     });
 
     /* 삭제 모달창 */
-    const modalOn2 = document.querySelector(".storage-delete");
     const modal2 = document.querySelector(".modal2");
     const closeBtn2 = document.querySelector(".cancel-btn");
+    const modal3 = document.querySelector(".modal3");
+    const closeBtn3 = document.querySelector(".cancel-btn2");
 
-    modalOn2.addEventListener("click", function() {
-        modal2.classList.add("show2");
+    document.querySelectorAll(".storage-delete").forEach((item) => {
+        item.addEventListener("click", () => {
+            modal2.classList.add("show2");
+        })
+    });
 
     closeBtn2.addEventListener("click", function() {
         modal2.classList.remove("show2")
-
     }) 
+
+    document.querySelectorAll(".del-list").forEach((item) => {
+        item.addEventListener("click", () => {
+            modal3.classList.add("show3");
+        })
+    });
+    closeBtn3.addEventListener("click", function() {
+        modal3.classList.remove("show3")
+    }) 
+
 
     /* 외부 클릭 시 모달창 닫기 */
     window.addEventListener("click", function(event) {
@@ -33,6 +47,9 @@ document.addEventListener("DOMContentLoaded", function () {
         if (event.target === modal2) {
             modal2.classList.remove("show2");
         }
+        if (event.target === modal3) {
+            modal3.classList.remove("show3");
+        }
     });
   });
-});
+
--- js/reservation/reservationSelectCompany.js
+++ js/reservation/reservationSelectCompany.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const tabs = document.querySelectorAll(".tab");
     const contents = document.querySelectorAll(".contents");
 
-    /* 첫 번째 탭 활성화 */
+    // 첫 번째 탭 활성화
     if (tabs.length > 0 && contents.length > 0) {
         tabs[0].classList.add("tab-selected");
         contents[0].classList.add("contents-active");
@@ -37,6 +37,36 @@ document.querySelectorAll(".purpose-btn").forEach((item) => {
         });
     });
 
+/* 스케줄 검색 */
+/* const radioButtons = document.querySelectorAll(".options input[type='radio']");
+const selectDateDiv = document.querySelector(".select-date");
+const daySelect = document.querySelector("#day");
+const dateInput = document.querySelector("#date");
+
+radioButtons.forEach(radio => {
+    radio.addEventListener("change", () => {
+        if (radio.value === "day") {
+            daySelect.style.display = "block";
+            dateInput.style.display = "none";
+        } else if (radio.value === "date") {
+            daySelect.style.display = "none";
+            dateInput.style.display = "block";
+        }
+    });
+});
+
+// 초기 상태 설정
+const checkedRadio = document.querySelector('input[name="hope-date"]:checked');
+if (checkedRadio) {
+    if (checkedRadio.value === "day") {
+        daySelect.style.display = "block";
+        dateInput.style.display = "none";
+    } else {
+        daySelect.style.display = "none";
+        dateInput.style.display = "block";
+    }
+}
+}); */
 
 /* 업체 카드 선택 */
 const cards = document.querySelectorAll(".company-card");
